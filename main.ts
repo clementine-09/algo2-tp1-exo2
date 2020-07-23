bc97edb32c7140475358bd16866ca4ae7e92ef9b
clementine-09 <68041989+clementine-09@users.noreply.github.com>
2020-07-24 09:10:39 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     led.unplot(0, posY)
-    posY = Math.max(0, posY - 1)
+    posX += direction
     led.plot(0, posY)
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(0, posY)
-    posY = Math.min(4, posY + 1)
-    led.plot(0, posY)
+    led.unplot(posX, posY)
+    posX += 1
+    if (true) {
+        posX = 0
+    }
+    led.plot(posX, posY)
 })
+let direction = 0
+let posX = 0
 let posY = 0
 posY = 0
-led.unplot(0, posY)
-let direction = 0
+posX = 0
+direction = 1
 basic.forever(function () {
     basic.pause(200)
-    posY += 1
+    led.unplot(posX, posY)
+    posY += direction
+    led.plot(posX, posY)
     if (posY >= 4) {
         direction = -1
     } else if (posY <= 0) {
